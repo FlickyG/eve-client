f8c8f61676a6ebd46c11f9562d15d99797085a6e
adam.green <adam.green@ocado.com>
2016-02-07 18:52:36 +0000

    added evecrest

---
+++ eveSQL/EVECrest.py
@@ -0,0 +1,207 @@
+'''
+Created on 30 Jan 2016
+
+@author: adam
+'''
+
+import pycrest, pprint, psycopg2, time, datetime
+from requests.exceptions import ConnectionError
+from pip._vendor.requests.packages.urllib3.exceptions import ProtocolError
+import requests, sys
+
+eve = pycrest.EVE()
+eve()
+
+def getByAttrVal(objlist, attr, val):
+    ''' Searches list of dicts for a dict with dict[attr] == val '''
+    matches = [getattr(obj, attr) == val for obj in objlist]
+    index = matches.index(True)  # find first match, raise ValueError if not found
+    return objlist[index]
+
+def getAllItems(page):
+    ''' Fetch data from all pages '''
+    ret = page().items
+    while hasattr(page(), 'next'):
+        page = page().next()
+        ret.extend(page().items)
+    return ret
+
+#region = getByAttrVal(eve.regions().items, 'name', 'Catch')
+
+#item = getByAttrVal(getAllItems(eve.itemTypes), 'name', 'Tritanium').href
+#getAllItems(region().marketSellOrders(type=item))
+
+#market = getByAttrVal(eve.prices(), 'name', 'Tritanium', 'Catch')
+
+
+pprint.pprint(eve)
+
+#10000042 metro
+#490 22omm vulcan
+#history = eve.get('https://public-crest.eveonline.com/market/10000042/types/34/history/')
+#pprint.pprint(history)
+
+#get date of last entry in table
+#if date older than two days ago, get history
+# for each entry in history older than today's date and younger than the last entry
+#    enter data into database
+# 
+
+class EVECrest(object):
+    def __init__(self):
+        self.connQ = psycopg2.connect("dbname='eveclient'  user='adam'")
+        self.currQ = self.connQ.cursor()
+
+    def fetch_market_history(self, typeid, regionid):
+        try:
+            data = eve.get("https://public-crest.eveonline.com/market/{rg}/types/{id}/history/".
+                       format(rg = regionid, id = typeid))
+            return data
+        except ConnectionError as e:
+            print ("e", e)
+            print ("e.args", e.args)
+            try:
+                if e.args[0].args[1].errno == 104:
+                    print ("eve crest connection reset by peer")
+                    raise
+                else:
+                    pass
+            except Exception as f: #we wrongly assumed it was a reset connectio error
+                try:
+                    if "BadStatusLine" in e.args[0][1]:
+                        print ("pycrest encountered bad status line")
+                        raise
+                    else:
+                        pass                  
+                        #return f # ion which case we aren't sure what the rror is
+                except:
+                    raise
+                    #return e 
+    
+    def enter_market_history(self, type, region, data):
+        try:
+            self.currQ.execute("INSERT INTO markethistory ("
+                          "type, "
+                          "region, " 
+                          "avg, "
+                          "date, "
+                          "high, "
+                          "low, "
+                          "ordercount,"
+                          "volume) "
+                          "VALUES ( "
+                          "{typ}, "
+                          "{rgn}, " 
+                          "{avg}, "
+                          "\'{dte}\', "
+                          "{hgh}, "
+                          "{low}, "
+                          "{cnt}, "
+                          "{vlm}) ".
+                          format(
+                                 typ = type,
+                                 rgn = region,
+                                 avg = data["avgPrice"],
+                                 dte = data["date"].split("T")[0],
+                                 hgh = data["highPrice"],
+                                 low = data["lowPrice"],
+                                 cnt = data["orderCount"],
+                                 vlm = data["volume"]                           
+                                 )
+                          )
+            self.connQ.commit()
+        except psycopg2.IntegrityError:
+            self.connQ.rollback()
+
+
+    def get_date_last_entry(self, id, regionid):
+        self.currQ.execute("SELECT date FROM markethistory "
+                      "WHERE (type = {it} AND region = {region}) "
+                      "ORDER BY date DESC "
+                      "LIMIT 1".
+                      format(it = id, region = regionid))
+        data = self.currQ.fetchone()
+        """ If the database is empty, it may be because we've never pulled this data down
+            It may also be because there is no data to pull, such as capital ships in high sec
+        """
+        try:
+            assert data[0] != type(None)
+        except TypeError:
+            print ("no data int the psql db for this item!")
+            try:
+                temp_results = self.fetch_market_history(id, regionid)
+                x = temp_results["items"]
+                #need to handle this better whern the connection is reset by peer, in which case the temp_results are none
+            except TypeError as e:
+                print ("temp results")
+                print ("the error", e)
+                return ("None")
+            if len(x) == 0:
+                # here we catch the case of capital ships in high sec mentioned above
+                print ("there is no market data for this item")
+                return None
+            else: #here we pull down data we haven't seen before
+                for datapoint in x:
+                    self.enter_market_history(id, regionid, datapoint)
+                return self.get_date_last_entry(id, regionid)
+        # this is business as usual - get any missing day's info
+        last_date = data[0]
+        # server side data is updated each day at midnight
+        if datetime.date.today() - last_date < datetime.timedelta(days = 2): # if the data is up to date
+            return data
+        else: # our data is not up to date
+            try:
+                # fetch data from crest api
+                x = self.fetch_market_history(id, regionid)["items"]
+            except:
+                return (last_date)  # crest api is flakey - if we can't contact the server now, return the last entry.  Any data is better than none
+            """  if the item hasn't traded since our last update, then there is no data to pass
+                this may cause an infinite loop if not caught
+            """
+            data_found  = False # use this to prevent infinite loop
+            for datapoint in x:
+                print (datapoint)
+                #print ("type of datapoint", type(datetime.datetime.strptime(datapoint["date"].split("T")[0], "%Y-%m-%d").date()))
+                # skip market entries if the data is before the last date seen in the database
+                if (datetime.datetime.strptime(datapoint["date"].split("T")[0], "%Y-%m-%d").date()
+                    > last_date):
+                    data_found = True
+                    print ("entering data")
+                    self.enter_market_history(id, regionid, datapoint)
+                else:
+                    pass # we already have this data
+            if data_found:
+                return self.get_date_last_entry(id, regionid)
+            else:
+                return last_date # in the case where the item has no current market orders, return the last data one was seen.
+
+def main():
+    #molden heath 10000028 
+    crest = EVECrest()
+    x = crest.get_date_last_entry(489, 10000042)
+    return (x) #nothing to do if you call this directly
+
+
+if __name__ == "__main__":
+    exit(main())
+
+        
+        
+        
+"""        
+        get_date_last_entry(id, regionid)
+
+
+def test(id, regionid):
+    self.currQ.execute("SELECT * FROM markethistory "
+                  "WHERE (type = {it} AND region = {region}) "
+                  "ORDER BY date DESC "
+                  "LIMIT 1".
+                  format(it = id, region = regionid))
+    data = self.currQ.fetchall()
+    return data
+
+""" 
+        
+
+
